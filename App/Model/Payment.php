6ee4c76cef57c590fa1833d59a12a1f2dfe5d27d
najmi04 <tjutnajmi@gmail.com>
2024-11-29 17:07:45 +0700

    edit class payment

--- App/Model/Payment.php
+++ App/Model/Payment.php
@@ -25,10 +25,16 @@ class Payment extends DB
         }
     }
 
-    public function getAllPayment()
+    public function getAllPayment($status = 'completed', $offset = 0, $limit = 10)
     {
         try {
-            $stmt = $this->db->prepare("SELECT * FROM payment");
+            $stmt = $this->db->prepare("SELECT * FROM payment WHERE payment_status = :status LIMIT :offset, :limit");
+            $stmt->bindValue(':status', $status);
+            $stmt->bindValue(':offset', (int)$offset);
+            $stmt->bindValue(':limit', (int)$limit);
+
+            $stmt->execute();
+
             $payments = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
             return [
@@ -40,19 +46,17 @@ class Payment extends DB
         }
     }
 
-    public function updatePayment($id_payment, $id_order, $payment_method, $payment_status, $payment_date, $total_price)
+    public function updatePayment($id_payment, $payment_method)
     {
         try {
-            $stmt = $this->db->prepare("UPDATE payment SET id_order = :id_order, payment_method = :payment_method, payment_status = :payment_status, payment_date = :payment_date, total_price = :total_price WHERE id_payment = :id_payment");
+            $stmt = $this->db->prepare("UPDATE payment SET payment_method = :payment_method WHERE id_payment = :id_payment");
             $stmt->bindParam(':id_payment', $id_payment);
-            $stmt->bindParam(':id_order', $id_order);
             $stmt->bindParam(':payment_method', $payment_method);
-            $stmt->bindParam(':payment_status', $payment_status);
-            $stmt->bindParam(':payment_date', $payment_date);
-            $stmt->bindParam(':total_price', $total_price);
             $stmt->execute();
-            return $stmt->rowCount() > 0;
-            return ["success" => true];
+            
+            if ($stmt->rowCount() > 0) {
+                return ["success" => true];
+            }
         } catch (\PDOException $e) {
             http_response_code(500);
             return ["message" => $e->getMessage()];
@@ -65,8 +69,10 @@ class Payment extends DB
             $stmt = $this->db->prepare("DELETE FROM payment WHERE id_payment = :id_payment");
             $stmt->bindParam(':id_payment', $id_payment);
             $stmt->execute();
-            return $stmt->rowCount() > 0;
-            return ["success" => true];
+
+            if ($stmt->rowCount() > 0) {
+                return ["success" => true];
+            }
         } catch (\PDOException $e) {
             http_response_code(500);
             return ["message" => $e->getMessage()];
